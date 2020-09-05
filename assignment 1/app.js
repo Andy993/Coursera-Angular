b89896d9dd658aebbc77479bb7d86f6634c45a9d
Andy993 <43788166+Andy993@users.noreply.github.com>
2020-09-05 16:17:40 +0530

    assignment 1

---
+++ assignment 1/app.js
@@ -0,0 +1,48 @@
+( function()
+{
+    'use strict'
+
+    angular.module('assignment1',[])
+    .controller('LunchCheckController', LunchCheckController);
+
+    LunchCheckController.$inject = ['$scope'];
+    function LunchCheckController($scope){
+        $scope.message = "";
+     
+    
+        $scope.check = function(){
+            
+            if(!$scope.name){
+                $scope.message = "Please enter data first";
+            }
+            else{
+                var food1 = $scope.name.replace(/\s/g, "").split(',');
+
+                var food = food1.filter(item => item);
+                if(food.length == 0)
+                {
+                    $scope.message = "Please enter data first";
+                }
+                else if (food.length <= 3 ){
+                    $scope.message = "Enjoy!";
+                }
+                 else {
+                    $scope.message = "Too much!";
+                }
+                console.log(food.length);
+            }
+
+
+
+            
+
+           
+
+        }
+
+
+    }
+
+   
+}
+)();
